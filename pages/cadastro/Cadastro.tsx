3fac2cfd1dc98df0949313a09fe1db3489528672
jsvictorias <juliasantos7652@gmail.com>
2025-06-04 23:27:41 -0300

    feat: página de cadastro

--- pages/cadastro/Cadastro.tsx
+++ pages/cadastro/Cadastro.tsx
@@ -1,9 +1,37 @@
+import { Input } from "@/components/input/Input";
+import { AppStackParamList } from "@/types/navigation";
+import { useNavigation } from "@react-navigation/native";
+import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import * as S from "./styles";
 
 export const Cadastro = () => {
-    return(
-        <S.CadastroContainer>
-            Cadastro
-        </S.CadastroContainer>
-    )
+  type CadastroScreenNavigationProp = NativeStackNavigationProp<
+    AppStackParamList,
+    "Login"
+  >;
+  const navigation = useNavigation<CadastroScreenNavigationProp>();
+  return (
+    <S.CadastroContainer>
+      <Input placeholder="Nome" secureTextEntry={false} />
+      <Input placeholder="Cadastro (escolha números)" secureTextEntry={false} />
+      <Input placeholder="Email" secureTextEntry={false} />
+      <Input placeholder="Senha" secureTextEntry={false} />
+      <Input placeholder="Confirmar Senha" secureTextEntry={false} />
+
+      <S.Button>
+        <S.ButtonText>Cadastrar</S.ButtonText>
+      </S.Button>
+
+      <S.RegisterText>Já tem cadastro?</S.RegisterText>
+      <S.RegisterButton>
+        <S.RegisterButtonText
+          onClick={() => {
+            navigation.navigate("Login");
+          }}
+        >
+          Entrar
+        </S.RegisterButtonText>
+      </S.RegisterButton>
+    </S.CadastroContainer>
+  );
 };
--- pages/cadastro/styles.ts
+++ pages/cadastro/styles.ts
@@ -7,3 +7,31 @@ export const CadastroContainer = styled.View`
   padding: 20px;
   background-color: #e6f4ff;
 `;
+export const Button = styled.TouchableOpacity`
+  background-color: #fff;
+  padding: 12px 32px;
+  border-radius: 30px;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 22px;
+  font-weight: bold;
+  padding: 5px 60px;
+`;
+export const RegisterText = styled.Text`
+  font-size: 14px;
+  margin-bottom: 15px;
+`;
+
+export const RegisterButton = styled.TouchableOpacity`
+  border: 4px solid #fff;
+  background-color: #d0eaff;
+  padding: 12px 32px;
+  border-radius: 30px;
+`;
+
+export const RegisterButtonText = styled.Text`
+  font-size: 18px;
+`;
